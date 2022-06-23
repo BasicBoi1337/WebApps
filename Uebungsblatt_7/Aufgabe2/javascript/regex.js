c099fe9ac43454c9b40e6366c76dd823a94ced63
BasicBoi1337 <manuel.kimmerle97@gmail.com>
2022-06-23 11:17:25 +0200

    Änderungen Aufgabe 2 Übungsblatt 7

--- Uebungsblatt_7/Aufgabe2/javascript/regex.js
+++ Uebungsblatt_7/Aufgabe2/javascript/regex.js
@@ -4,24 +4,22 @@ submit.addEventListener("click", function(evt){
     let inhaltName = document.getElementById("name").value;
     let inhaltAnmerkungen = document.getElementById("anmerk").value;
 
-    let regexname = /^[a-z ,.'-]+$/i;
+    // Regex direkt erzeugt und verglichen 
+    let regexname = /^[a-z ,.'-]+$/
 
+    // Neues Regex Objekt erzeugt und erst dann
     let regExpForAnmerkungen = new RegExp("<[^>]*>");
 
     let testName = regexname.test(inhaltName);
     let testAnmerkungen = regExpForAnmerkungen.test(inhaltAnmerkungen);
-
-    if(testAnmerkungen){
-        evt.preventDefault();
-        window.alert("Keine Codesnippets in den Anmerkungen erlaubt!");
-    }
 
     if(!testName){
         evt.preventDefault();
         window.alert("Name wurde falsch eingegeben!");
     }
 
-    
-
-
+    if(testAnmerkungen){
+        evt.preventDefault();
+        window.alert("Keine Codesnippets in den Anmerkungen erlaubt!");
+    }
 });
