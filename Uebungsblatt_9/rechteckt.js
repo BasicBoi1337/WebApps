b4d9bd4042ff61a9e28f3ea21a7b183d58084fa0
BasicBoi1337 <BasicBoi1337@users.noreply.github.com>
2022-07-07 18:21:59 +0200

    Übungsblatt 9 Aufgabe 3 fertig

--- Uebungsblatt_9/rechteckt.js
+++ Uebungsblatt_9/rechteckt.js
@@ -1,13 +1,44 @@
-let Rechteck = function(top, left, width, height, color){
-    this._top = top;
-    this._left = left;
-    this._width = width;
-    this._height = height;
-    this.color = color;
+class Rechteck {
+    constructor(left, top, width, height, color) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(context) {
+        context.fillStyle = this.color;
+        context.fillRect(this.left, this.top, this.width, this.height);
+    }
 }
 
-Rechteck.prototype.draw(context){
-    this._context = context;
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * (max + 1));
 };
 
-let viereck = new Rechteck(5, 5, 5, 5, )
+const rectangle = new Rechteck(50, 5, 200, 100, "red");
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+const btnDelete = document.getElementById("delete");
+
+const rectangles = [];
+rectangles.push(rectangle);
+
+rectangle.draw(context);
+
+canvas.onclick = (e) => {
+    const rectangle = new Rechteck(
+        e.layerX,
+        e.layerY,
+        getRandomInt(canvas.width - e.layerX),
+        getRandomInt(canvas.height - e.layerY),
+        `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`
+    );
+    rectangle.draw(context);
+    rectangles.push(rectangle);
+};
+
+btnDelete.onclick = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+};
